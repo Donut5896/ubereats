8c93fc4a72d104c2b3e36051787001e7f22f671e
Nutchanokchon Pooyatorn <nutchanokchon5@gmail.com>
2021-03-04 23:35:14 +0700

    4/3/21

--- main.js
+++ main.js
@@ -1,10 +1,16 @@
 const toggleBtn = document.querySelector(".toggle");
-const sidebar = document.querySelector(".sideBar");
-const closeBtn = document.querySelector(".close-btn");
+const sidebar = document.getElementById("sideBar");
+const overlay = document.getElementById("myOverlay");
 
 
 toggleBtn.addEventListener("click", function(){
-    sidebar.classList.toggle("show");
+    sidebar.style.display = "block";
+    overlay.style.display = "block";
 
-})
+});
 
+overlay.addEventListener("click", function(){
+    sidebar.style.display = "none";
+    overlay.style.display = "none";
+
+});
