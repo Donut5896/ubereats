9eed8a572b8dde645d4a39eb6266e75614514293
Nutchanokchon Pooyatorn <nutchanokchon5@gmail.com>
2021-03-10 23:23:16 +0700

    -

--- main.js
+++ main.js
@@ -3,6 +3,11 @@ const sidebar = document.getElementById("sideBar");
 const overlay = document.getElementById("myOverlay");
 
 
+
+
+
+
+
 toggleBtn.addEventListener("click", function(){
     sidebar.style.display = "block";
     overlay.style.display = "block";
@@ -14,3 +19,36 @@ overlay.addEventListener("click", function(){
     overlay.style.display = "none";
 
 });
+
+
+/* national brands section */
+
+window.onload = () => {
+    const brands = Array.from(document.getElementsByClassName('brands'));
+    console.log(brands);
+
+    //loop through listing
+    brands.forEach(brand => handleSlider(brand));
+}
+
+const handleSlider = brand => {
+    //get the first element of the array [0]
+   //const list = document.getElementByClassName('brand')[0]
+    const arrowLeft = document.getElementByClassName('arrow-left')[0]
+    const arrowRight = document.getElementByClassName('arrow-right')[0]
+
+
+    if(!arrowLeft || !arrowRight){
+        console.log(brand);
+        return 
+    }
+
+    arrowRight.addEventListener('click', e => {
+        e.preventDefault()
+        brand.scrollTo({
+            left: brand.offsetWidth,
+            behaviour: 'smooth'
+
+        })
+    })
+}
