1848f3e48934177662d286deb12446b5f65e0107
Nutchanokchon Pooyatorn <nutchanokchon5@gmail.com>
2021-03-11 22:56:16 +0700

    slider fixed

--- main.js
+++ main.js
@@ -33,22 +33,48 @@ window.onload = () => {
 
 const handleSlider = brand => {
     //get the first element of the array [0]
-   //const list = document.getElementByClassName('brand')[0]
-    const arrowLeft = document.getElementByClassName('arrow-left')[0]
-    const arrowRight = document.getElementByClassName('arrow-right')[0]
+    const list = brand.getElementsByClassName('brand')[0];
+    const arrowLeft = brand.getElementsByClassName('arrow-left')[0];
+    const arrowRight = brand.getElementsByClassName('arrow-right')[0];
 
 
-    if(!arrowLeft || !arrowRight){
+    if(!list || !arrowLeft || !arrowRight){
         console.log(brand);
         return 
     }
 
     arrowRight.addEventListener('click', e => {
-        e.preventDefault()
-        brand.scrollTo({
-            left: brand.offsetWidth,
-            behaviour: 'smooth'
+        e.preventDefault();
+
+        hanndleClassChange('arrow-right');
+
+        list.scrollTo({
+            left: list.offsetWidth,
+            behavior: 'smooth',
 
         })
     })
+    arrowLeft.addEventListener('click', e => {
+        e.preventDefault();
+        
+        hanndleClassChange('arrow-left');
+
+        list.scrollTo({
+            left: 0,
+            behavior: 'smooth',
+
+        })
+    })
+
+    // class change button
+    const hanndleClassChange = direction => {
+        
+        if(direction == 'arrow-right'){
+            arrowRight.classList.remove('darker');
+            arrowLeft.classList.add('darker');
+        }else if(direction == "arrow-left"){
+            arrowLeft.classList.remove('darker');
+            arrowRight.classList.add('darker');
+        }
+    }
 }
